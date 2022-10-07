ce20a18cfdd23d348eef01f1ff54ac722cca23b3
Phillip <phillip.lam0@gmail.com>
2022-10-07 12:55:22 -0400

    fix: updated dark mode on input component

--- my-app/components/Input.tsx
+++ my-app/components/Input.tsx
@@ -6,14 +6,20 @@ export default function Input() {
   const [darkMode, setDarkMode] = useContext(DarkModeContext);
 
   return (
-    <div className="h-[60px] w-[350px] px-7 flex gap-2 align-items items-center bg-white shadow-md rounded-lg">
+    <div
+      className={`h-[60px] w-[350px] px-7 flex gap-2 align-items items-center shadow-md rounded-lg ${
+        darkMode ? "text-white bg-dark-blue" : "text-very-dark-blue-lm bg-white"
+      }`}
+    >
       <HiOutlineSearch className="text-gray-400" />
       <input
         type="text"
         id="search"
         name="search"
         placeholder={`Search for a country...`}
-        className={"w-full focus:outline-none focus:border-blue-500 bg-inherit"}
+        className={`w-full focus:outline-none ${
+          darkMode ? "bg-dark-blue" : "bg-white"
+        }`}
       />
     </div>
   );
