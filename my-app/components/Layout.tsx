029e3a33b1c005ed8b2ec124bd551df00db666b0
Phillip <phillip.lam0@gmail.com>
2022-10-07 11:43:36 -0400

    fix: moving dark mode trigger to Layout component

--- my-app/components/Layout.tsx
+++ my-app/components/Layout.tsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
+import { DarkModeContext } from "../DarkModeContext";
 import Nav from "./Nav";
 
 export default function Layout({ children }: any) {
-  //TODO
+  const [darkMode, setDarkMode] = useContext(DarkModeContext);
+
   return (
     <div>
       <Nav />
-      <main>{children}</main>
+      <main
+        className={`px-4 ${
+          darkMode ? "bg-very-dark-blue-bg" : "bg-very-light-gray"
+        }`}
+      >
+        {children}
+      </main>
     </div>
   );
 }
