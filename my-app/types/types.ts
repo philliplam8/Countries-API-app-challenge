1c8329481b03b3805a90aab94daa547c1effaedc
Phillip <phillip.lam0@gmail.com>
2022-10-11 12:58:47 -0400

    fix: updated Country interface to include optional properties

--- my-app/types/types.ts
+++ my-app/types/types.ts
@@ -2,7 +2,12 @@ export interface Country {
   key: string;
   flagImage: string;
   countryName: string;
+  nativeName?: string;
   population: string;
   region: string;
+  subRegion?: string;
   capital: string;
+  topLevelDomain?: string;
+  currencies?: string;
+  languages?: string;
 }
