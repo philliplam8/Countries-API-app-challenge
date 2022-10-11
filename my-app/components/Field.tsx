9b45c0b06e611e84e19eed5080cfe95a15e4f3d6
Phillip <phillip.lam0@gmail.com>
2022-10-11 14:48:52 -0400

    chore: adding typescript type to props

---
+++ my-app/components/Field.tsx
@@ -0,0 +1,10 @@
+import { ReadOnlyField } from "../types/types";
+
+export default function Field(props: ReadOnlyField) {
+  return (
+    <div className="flex gap-2">
+      <h3 className="font-semibold">{props.fieldName}</h3>
+      <p className="font-light">{props.fieldValue}</p>
+    </div>
+  );
+}
