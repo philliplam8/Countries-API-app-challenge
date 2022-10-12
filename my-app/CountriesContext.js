8fe8c9ec50d0ed24d39cdd5398d3d3e72a48484d
Phillip <phillip.lam0@gmail.com>
2022-10-12 00:36:33 -0400

    fix: keeping context as JS

--- my-app/CountriesContext.js
+++ my-app/CountriesContext.js
@@ -1,13 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
+
+// type Props = {
+//   children: React.ReactNode;
+// };
+
+// type Context = {
+//   countries: [];
+//   setCountries: Dispatch<SetStateAction<Context>>;
+// };
+
+// const initialContext: Context = {
+//   countries: [],
+//   setCountries: (): void => {
+//     throw new Error("setCountries function must be overridden");
+//   },
+// };
+
+// export const CountriesContext = createContext<Context>(initialContext);
+
+// export const CountriesProvider = ({ children }: Props): JSX.Element => {
+//   const [countries, setCountries] = useState<Context>(initialContext);
+
+//   return (
+//     <CountriesContext.Provider value={{ ...countries, setCountries }}>
+//       {children}
+//     </CountriesContext.Provider>
+//   );
+// };
 
 export const CountriesContext = createContext();
 
-export const CountriesProvider = props => {
-    const [countries, setCountries] = useState([]);
+export const CountriesProvider = (props) => {
+  const [countries, setCountries] = useState([]);
 
-    return (
-        <CountriesContext.Provider value={[countries, setCountries]}>
-            {props.children}
-        </CountriesContext.Provider>
-    )
-}
+  return (
+    <CountriesContext.Provider value={[countries, setCountries]}>
+      {props.children}
+    </CountriesContext.Provider>
+  );
+};
--- my-app/DarkModeContext.js
+++ my-app/DarkModeContext.js
@@ -1,3 +1,33 @@
+// import { createContext, Dispatch, SetStateAction, useState } from "react";
+
+// type Props = {
+//   children: React.ReactNode;
+// };
+
+// type Context = {
+//   darkMode: boolean;
+//   setDarkMode: Dispatch<SetStateAction<Context>>;
+// };
+
+// const initialContext: Context = {
+//   darkMode: false,
+//   setDarkMode: (): void => {
+//     throw new Error("setDarkMode function must be overridden");
+//   },
+// };
+
+// export const DarkModeContext = createContext<Context>(initialContext);
+
+// export const DarkModeProvider = ({ children }: Props): JSX.Element => {
+//   const [darkMode, setDarkMode] = useState<Context>(initialContext);
+
+//   return (
+//     <DarkModeContext.Provider value={{ ...darkMode, setDarkMode }}>
+//       {children}
+//     </DarkModeContext.Provider>
+//   );
+// };
+
 import { createContext, useState } from 'react';
 
 export const DarkModeContext = createContext();
