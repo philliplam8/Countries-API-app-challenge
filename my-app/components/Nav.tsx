5de356aaf9afa4c4d7f7419914877329c84348bc
Phillip <phillip.lam0@gmail.com>
2022-10-12 15:33:24 -0400

    feat: fix navbar at the top of site

--- my-app/components/Nav.tsx
+++ my-app/components/Nav.tsx
@@ -13,7 +13,7 @@ export default function Nav() {
 
   return (
     <nav
-      className={`py-8 lg:px-20 flex justify-between items-center shadow-md ${
+      className={`sticky top-0 z-10 w-full py-8 lg:px-20 flex justify-between items-center shadow-md ${
         darkMode ? "text-white bg-dark-blue" : "text-very-dark-blue-lm bg-white"
       }`}
     >
