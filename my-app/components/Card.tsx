f3dd2cb56cafac90fc0f12b6eebc05d4db008b70
Phillip <phillip.lam0@gmail.com>
2022-10-12 16:04:16 -0400

    fix: adding more spacing for long country names

--- my-app/components/Card.tsx
+++ my-app/components/Card.tsx
@@ -14,17 +14,20 @@ export default function Card(props: Country) {
 
   return (
     <div
-      className={`h-[350px] w-[300px] rounded-lg shadow-md cursor-pointer
+      className={`h-[390px] w-[300px] rounded-lg shadow-md cursor-pointer
       ${
         darkMode ? "text-white bg-dark-blue" : "text-very-dark-blue-lm bg-white"
       }`}
       onClick={cardClickHandler}
     >
       <div className="card-image h-[200px] shadow-sm">
-        <img src={props.flagImage} className="h-full w-[300px] object-fit" />
+        <img
+          src={props.flagImage}
+          className="h-full w-[300px] object-fit rounded-t-lg"
+        />
       </div>
 
-      <div className="card-details m-6">
+      <div className="card-details mx-7 my-6">
         <h2 className="font-extrabold my-4 text-lg">{props.countryName}</h2>
         <div className="card-country-details flex flex-col gap-1 text-sm">
           <Field fieldName="Population:" fieldValue={props.population} />
